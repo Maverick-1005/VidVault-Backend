6f786f9012095b67815090102d9df5ff887f6229
Ansh Mishra <anshmishra@Anshs-MacBook-Air.local>
2024-12-14 17:47:55 +0530

    controllers and routers setup

--- src/app.js
+++ src/app.js
@@ -7,19 +7,22 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));  // Explore more options in Documentation
-
 app.use(express.json({
     limit: "16kb"   // some codebases have body parser for this
 }))
-
 app.use(express.urlencoded({
     extended: true,
     limit: "16kb"
 }))
-
 app.use(express.static("public"))
-
 app.use(cookieParser())
 
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+
+app.use("/api/v1/users" , userRouter)
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
@@ -13,7 +14,7 @@ connectDB()
    })
 
     app.listen(process.env.PORT || 8000 , () => {
-        console.log(`Serve is running at port : ${process.env.PORT}`)
+        console.log(`Server  is running at port : ${process.env.PORT}`)
     })
 })
 .catch( (err) => {
