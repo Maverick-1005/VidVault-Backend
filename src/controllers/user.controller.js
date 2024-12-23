f05db9557183ff3a73dac329891794124cb7214b
Ansh Mishra <anshmishra@Anshs-MacBook-Air.local>
2024-12-23 18:49:38 +0530

    routes setup for watchHistory and all

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 const generateAccessAndRefreshToken = async(userId) => {
     try{
      const user =  await User.findById(userId);
@@ -382,18 +383,70 @@ const getUserChannelProfile = asyncHandler( async(req , res) => {
             }
         }
     ])
+
+    if(!channel?.length){
+        throw new ApiError(404 , "No such channel found")
+     }
+    
+     return res
+     .status(200)
+     .json(new ApiResponse (200 , channel[0] , "userChannel fetched successfully "))
+    
+    
 })
-
- if(!channel?.length){
-    throw new ApiError(404 , "No such channel found")
- }
-
- return res
- .status(200)
- .json(new ApiResponse (200 , channel[0] , "userChannel fetched successfully "))
 
 // console log krke dekhna channel
 
+const getWatchHistory = asyncHandler( async(req,res) => {
+    const useer = await User.aggregate([
+        {
+            $match: {
+                _id:  new mongoose.Types.ObjectId(req.user._id)      // yaha mongoose apna kaam nahi krega string ko compare
+            }
+        }, 
+        {
+            $lookup: {
+                from : "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                },
+                                {
+                                    $addFields:{
+                                        owner:{
+                                            $first: "owner"
+                                        }
+                                    }
+                                }
+                            ]
+
+                        }
+
+                    }
+                ]
+
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200 , user[0].watchHistory , "Watch History fetched succcessfully " ))
+})
 
 export {registerUser ,
      loginUser , 
@@ -403,7 +456,9 @@ export {registerUser ,
      changeCurrentPassword , 
      updateAccountDetails,
      updateUserAvatar,
-     updateUserCoverImage
+     updateUserCoverImage,
+     getUserChannelProfile,
+     getWatchHistory
     }
 
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { registerUser , loginUser, logoutUser , refreshAccessToken} from "../controllers/user.controller.js";
+import { 
+  registerUser , 
+  loginUser,
+   logoutUser , 
+   refreshAccessToken , 
+   changeCurrentPassword,
+    getCurrentUser,
+     updateAccountDetails, 
+     updateUserAvatar, 
+     updateUserCoverImage, 
+     getUserChannelProfile,
+     getWatchHistory
+    } 
+    from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -29,4 +42,24 @@ userRouter.route("/logout").post(
 userRouter.route("/refresh-token").post(
   refreshAccessToken
 )
+userRouter.route("/change-password").post(
+  verifyJWT , 
+  changeCurrentPassword
+)
+userRouter.route("/current-user").get(
+  verifyJWT, getCurrentUser
+)
+// why patch
+userRouter.route("/update-account").patch(
+  verifyJWT , updateAccountDetails
+)
+
+userRouter.route("/avatar").patch(verifyJWT , upload.single("avatar") , updateUserAvatar)
+
+userRouter.route("/coverImage").patch(verifyJWT , upload.single("coverImage") , updateUserCoverImage )
+
+userRouter.route("/c/:username").get(
+  verifyJWT , getUserChannelProfile
+)
+userRouter.route("/u/history").get(verifyJWT , getWatchHistory)
 export default userRouter
