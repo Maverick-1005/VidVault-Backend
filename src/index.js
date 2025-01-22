6077b09cb45b7cd47a4e094a2b25e6d29e65f9e5
Ansh Mishra <anshmishra@Anshs-MacBook-Air.local>
2025-01-23 00:50:14 +0530

    added the redirect URI

--- src/index.js
+++ src/index.js
@@ -21,21 +21,3 @@ connectDB()
     console.log("Mongo Db connection failed !!!" , err)
 })
 
-
-// iffi
-// ;( async () => {
-//     try{
-//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    //   app.on("error" , (err) => {
-    //        console.log("Error: " , err);
-    //        throw err
-    //   })
-
-//       app.listen(process.env.PORT , () => {
-//         console.log(`App is listening on port ${process.env.PORT}`);
-//       })
-//     }
-//     catch(err){
-//         console.log("ERROR: " , err);
-//     }
-// })()
--- src/utils/googleConfig.js
+++ src/utils/googleConfig.js
@@ -6,5 +6,5 @@ export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 export const oauth2client = new google.auth.OAuth2(
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
-    'postmessage'
+    "https://be2b24b4-17b5-46f4-844f-86d937295ae3.e1-us-east-azure.choreoapps.dev/api/v1/users/auth/google"
 )
